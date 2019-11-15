5ad43d080e080891ceda251f361c053657f27f58
Heikki Kangas <heikki.kangas@hotmail.fi>
2019-11-15 18:10:21 +0200

    changed copy path to relative

--- move-spec.sh
+++ move-spec.sh
@@ -1,1 +1,1 @@
-cp -r ./typescript-client-generated/api /mnt/c/ojube-git/hacky-server/src/rest && cp -r ./typescript-client-generated/model /mnt/c/ojube-git/hacky-server/src/rest
+cp -r ./typescript-client-generated/api ../hacky-server/src/rest && cp -r ./typescript-client-generated/model ../hacky-server/src/rest
