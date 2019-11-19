e62c2196842d1cf9c71fcec7edaea767d1f36d5f
ojube <julien.9392@gmail.com>
2019-11-19 12:58:13 +0200

    minor change to sh

--- move-unity-client.sh
+++ move-unity-client.sh
@@ -1,1 +1,1 @@
-cp -r ./unity-csharp-client-generated/src/IO.Swagger/Api /mnt/c/UnityProjects/weberquest/Assets/Rest && cp -r ./unity-csharp-client-generated/src/IO.Swagger/Model /mnt/c/UnityProjects/weberquest/Assets/Rest
+cp -r ./unity-csharp-client-generated/src/IO.Swagger/Api /mnt/c/UnityProjects/Hacky/Assets/Rest && cp -r ./unity-csharp-client-generated/src/IO.Swagger/Model /mnt/c/UnityProjects/Hacky/Assets/Rest
